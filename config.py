0cc315c7d5f518bb83fe3ae7c2372031e5260cee
Petr Nuzhnov <petr.nuzhnov@gmail.com>
2015-08-30 23:36:37 +0300

    Updates for testing

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 BOT_TOKEN = "" #insert here your bot token
 URL = "https://api.telegram.org/bot%s/" % BOT_TOKEN
+CHAT_ID = 0 #id of chat with test user
+BOT_NAME = ""#botname
--- test/tests.py
+++ test/tests.py
@@ -1,10 +1,26 @@
-import piebot.app.telegram_bot_api
+import app.telegram_bot_api as tele
+import config_test
 import unittest
 
-class BaseTest(unittest.TestCase):
+class ApiTest(unittest.TestCase):
 
-	def test_add(self):
-		self.assertEqual(100, 100 + 20)
+	def setUp(self):
+		self.url = config_test.URL
+		self.bot_name = config_test.BOT_NAME
+		self.chat_id = config_test.CHAT_ID
+
+	def test_connection(self):
+		self.assertEqual(tele.getVerifyBot(self.url), self.bot_name)
+
+	def test_get_updates(self):
+		data = tele.getMessages(self.url)
+		self.assertTrue(data)
+
+	def test_send_message(self):
+		text_string = "this is the test text"
+		data = tele.sendMessage(self.url, self.chat_id, text_string)
+		self.assertEquals(text_string, data["text"])
+
 
 if __name__ == '__main__':
 	unittest.main()
